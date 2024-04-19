23b06dcdb100814e62ca30132fca88feaeccca26
Tim Whitehead <tim@thescavengers.dev>
2024-04-19 07:14:55 -0500

    sendData() Structure

--- handler.js
+++ handler.js
@@ -1,5 +1,40 @@
 const http = require("https")
 
+let playing = false
+let frame = 0
+let fps = 30
+
+let initialFrameSend = 15
+let frameSend = 5
+
+async function sendData(node, data) {
+    let options = {
+        method: 'POST',
+        'Content-Type': 'application/json',
+    }
+
+    let result = ''
+    const req = http.request(node, options, (res) => {
+        console.log(res.statusCode)
+
+        res.setEncoding('utf8')
+        res.on('data', (chunk) => {
+            result += chunk
+        })
+
+        res.on('end', () => {
+            console.log(result)
+        })
+    })
+
+    req.on('error', (e) => {
+        console.error(e)
+    })
+
+    req.write(data)
+    req.end()
+}
+
 function loop() {
     setInterval(() => {    // Start code that will run over and over again as fast as possible
         // Do loop code here
